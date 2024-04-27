7ce1278b866ca2ad1702d135904b7e3a2d7b58c8
Scot Bontrager <cloudkucooland@gmail.com>
2024-04-27 15:48:04 -0500

    make this useful

--- webui/src/routes/blower/[id]/+page.svelte
+++ webui/src/routes/blower/[id]/+page.svelte
@@ -1,55 +1,85 @@
 <script>
-	import { onMount } from 'svelte';
-	import { invalidateAll } from '$app/navigation';
 	import {
 		Table,
 		TableBody,
-		TableBodyCell,
 		TableBodyRow,
-		TableHead,
-		TableHeadCell
+		TableBodyCell,
+		Heading,
+		P,
+		A,
+		Hr
 	} from 'flowbite-svelte';
-	import { Heading, P, A, Hr } from 'flowbite-svelte';
 
 	export let data;
-
-	onMount(() => {
-		const interval = setInterval(() => {
-			invalidateAll();
-		}, 30000);
-
-		return () => {
-			clearInterval(interval);
-		};
-	});
+	function parseTime(t) {
+		const d = new Date(t);
+		return d.toString();
+	}
+	function parseDuration(d) {
+		let duration = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+		const days = Math.floor(d / 86400);
+		if (days >= 1) {
+			duration.days = days;
+			d -= days * 86400;
+		}
+		const hours = Math.floor(d / 3600);
+		if (hours >= 1) {
+			duration.hours = hours;
+			d -= hours * 3600;
+		}
+		const minutes = Math.floor(d / 60);
+		if (minutes >= 1) {
+			duration.minutes = minutes;
+			d -= minutes * 60;
+		}
+		duration.seconds = d;
+		// browsers don't support Intl.DurationFormat() yet
+		return `${duration.days} days; ${duration.hours}:${duration.minutes}:${duration.seconds}`;
+	}
 </script>
 
 <Heading tag="h2">Blower {data.ID}: {data.Name}</Heading>
 <Table>
-	<TableHead>
-		<TableHeadCell>ID</TableHeadCell>
-		<TableHeadCell>Name</TableHeadCell>
-		<TableHeadCell>Hot Loop</TableHeadCell>
-		<TableHeadCell>Cold Loop</TableHeadCell>
-		<TableHeadCell>Running</TableHeadCell>
-		<TableHeadCell>Current Start Time</TableHeadCell>
-		<TableHeadCell>Last Start Time</TableHeadCell>
-		<TableHeadCell>Last Stop Time</TableHeadCell>
-		<TableHeadCell>Total Run Time</TableHeadCell>
-		<TableHeadCell>Filter Time</TableHeadCell>
-	</TableHead>
 	<TableBody>
 		<TableBodyRow>
+			<TableBodyCell>ID</TableBodyCell>
 			<TableBodyCell><A href="/blower/{data.ID}">{data.ID}</A></TableBodyCell>
+		</TableBodyRow>
+		<TableBodyRow>
+			<TableBodyCell>Name</TableBodyCell>
 			<TableBodyCell>{data.Name}</TableBodyCell>
+		</TableBodyRow>
+		<TableBodyRow>
+			<TableBodyCell>Hot Loop</TableBodyCell>
 			<TableBodyCell><A href="/loop/{data.HotLoop}">{data.HotLoop}</A></TableBodyCell>
+		</TableBodyRow>
+		<TableBodyRow>
+			<TableBodyCell>Cold Loop</TableBodyCell>
 			<TableBodyCell><A href="/loop/{data.ColdLoop}">{data.ColdLoop}</A></TableBodyCell>
+		</TableBodyRow>
+		<TableBodyRow>
+			<TableBodyCell>Running</TableBodyCell>
 			<TableBodyCell>{data.Running}</TableBodyCell>
-			<TableBodyCell>{data.CurrentStartTime}</TableBodyCell>
-			<TableBodyCell>{data.LastStartTime}</TableBodyCell>
-			<TableBodyCell>{data.LastStopTime}</TableBodyCell>
-			<TableBodyCell>{data.Runtime}</TableBodyCell>
-			<TableBodyCell>{data.FilterTime}</TableBodyCell>
+		</TableBodyRow>
+		<TableBodyRow>
+			<TableBodyCell>Current Start Time</TableBodyCell>
+			<TableBodyCell>{parseTime(data.CurrentStartTime)}</TableBodyCell>
+		</TableBodyRow>
+		<TableBodyRow>
+			<TableBodyCell>Last Start Time</TableBodyCell>
+			<TableBodyCell>{parseTime(data.LastStartTime)}</TableBodyCell>
+		</TableBodyRow>
+		<TableBodyRow>
+			<TableBodyCell>Last Stop Time</TableBodyCell>
+			<TableBodyCell>{parseTime(data.LastStopTime)}</TableBodyCell>
+		</TableBodyRow>
+		<TableBodyRow>
+			<TableBodyCell>Total Run Time</TableBodyCell>
+			<TableBodyCell>{parseDuration(data.Runtime)}</TableBodyCell>
+		</TableBodyRow>
+		<TableBodyRow>
+			<TableBodyCell>Filter Time</TableBodyCell>
+			<TableBodyCell>{parseDuration(data.FilterTime)}</TableBodyCell>
 		</TableBodyRow>
 	</TableBody>
 </Table>
