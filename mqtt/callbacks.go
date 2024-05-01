e7bc18c63716ce343ca229ec6262fb66a652c55d
Scot Bontrager <cloudkucooland@gmail.com>
2024-05-01 10:10:41 -0500

    don't log the routine checkins, stop pump if last blower on loop is stopped

--- mqtt/callbacks.go
+++ mqtt/callbacks.go
@@ -33,18 +33,39 @@ func blowerCallbackFn(cl *mqtt.Client, sub packets.Subscription, pk packets.Pack
 		return
 	}
 
-	log.Info("blower state change", "blower", bn, "state", response.CurrentState)
-	blower.Running = response.CurrentState
-	if !response.CurrentState && response.RanTime > 0 {
-		blower.Runtime += response.RanTime
-	}
-	if response.CurrentState {
-		// now running, log start time
-		blower.CurrentStartTime = time.Now()
-	} else {
-		// now stopped, log stop time
-		blower.LastStopTime = time.Now()
-		blower.LastStartTime = blower.CurrentStartTime
+	// ignore the routine check-ins if no change
+	if response.CurrentState != blower.Running {
+		log.Info("blower state change", "blower", bn, "state", response.CurrentState)
+		blower.Running = response.CurrentState
+		if !response.CurrentState && response.RanTime > 0 {
+			blower.Runtime += response.RanTime
+		}
+		if response.CurrentState {
+			// now running, log start time
+			blower.CurrentStartTime = time.Now()
+		} else {
+			// now stopped, log stop time
+			blower.LastStopTime = time.Now()
+			blower.LastStartTime = blower.CurrentStartTime
+		}
+
+		c := hvac.GetConfig()
+		if c.SystemMode == hvac.SystemModeCool {
+			blowerRunning := false
+			for k := range c.Blowers {
+				if blower.ColdLoop == c.Blowers[k].ColdLoop && c.Blowers[k].Running {
+					blowerRunning = true
+				}
+			}
+			if !blowerRunning {
+				log.Info("last blower on cool loop stopped, shutting down pump for the loop")
+				for k := range c.Pumps {
+					if c.Pumps[k].Loop == blower.ColdLoop {
+						c.Pumps[k].ID.Stop("internal")
+					}
+				}
+			}
+		}
 	}
 }
 
@@ -68,19 +89,22 @@ func pumpCallbackFn(cl *mqtt.Client, sub packets.Subscription, pk packets.Packet
 		return
 	}
 
-	log.Info("pump state change", "pump", pn, "state", response.CurrentState)
-	pump.Running = response.CurrentState
-	if !response.CurrentState && response.RanTime > 0 {
-		pump.Runtime += response.RanTime
-	}
+	// ignore the routine check-ins if no change
+	if response.CurrentState != pump.Running {
+		log.Info("pump state change", "pump", pn, "state", response.CurrentState)
+		pump.Running = response.CurrentState
+		if !response.CurrentState && response.RanTime > 0 {
+			pump.Runtime += response.RanTime
+		}
 
-	if response.CurrentState {
-		// now running, log start time
-		pump.CurrentStartTime = time.Now()
-	} else {
-		// now stopped, log stop time
-		pump.LastStopTime = time.Now()
-		pump.LastStartTime = pump.CurrentStartTime
+		if response.CurrentState {
+			// now running, log start time
+			pump.CurrentStartTime = time.Now()
+		} else {
+			// now stopped, log stop time
+			pump.LastStopTime = time.Now()
+			pump.LastStartTime = pump.CurrentStartTime
+		}
 	}
 }
 
