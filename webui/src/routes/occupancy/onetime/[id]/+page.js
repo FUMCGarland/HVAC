07d297e585402b38daa228bc72513d49433ab041
Scot Bontrager <cloudkucooland@gmail.com>
2024-05-11 20:50:55 -0500

    format

---
+++ webui/src/routes/occupancy/onetime/[id]/+page.js
@@ -0,0 +1,15 @@
+import { hvaccontroller } from '$lib/hvac.js';
+
+export async function load({ fetch, params }) {
+	const res = await fetch(`${hvaccontroller}/api/v1/occupancy`);
+	const all = await res.json();
+
+	const filt = all.Recurring.filter((r) => params.id == r.ID);
+	const item = filt[0];
+
+	const sys = await fetch(`${hvaccontroller}/api/v1/system`);
+	const system = await sys.json();
+	item.SystemRooms = system.Rooms;
+
+	return item;
+}
