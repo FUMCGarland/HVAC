2a7db066119a762d7cd348fa4aa9f0db3bc8ca53
Scot Bontrager <cloudkucooland@gmail.com>
2024-05-12 22:21:43 -0500

    tweaks and cleanup

--- consts.go
+++ consts.go
@@ -8,7 +8,6 @@ type DegF uint8
 
 const defaultRunDuration time.Duration = (1 * time.Hour)
 
-// TODO: implement this
 const MaxPumpRunTime time.Duration = (6 * time.Hour)
 const MinPumpRunTime time.Duration = (30 * time.Minute)
 const MaxBlowerRunTime time.Duration = (6 * time.Hour)
@@ -19,12 +18,13 @@ const MinChillerRunTime time.Duration = (30 * time.Minute)
 const pumpMinTimeBetweenRuns time.Duration = (5 * time.Minute)
 const blowerMinTimeBetweenRuns time.Duration = (5 * time.Minute)
 const chillerMinTimeBetweenRuns time.Duration = (5 * time.Minute)
-const minZoneTemp DegF = 55
-const maxZoneTemp DegF = 85
 
-// this is the "I don't know what I'm doing" part of the project
+const minZoneTemp DegF = 60 // the coolest we will accept as a user-defined value
+const maxZoneTemp DegF = 80 // the warmest we will accept as a user-defined value
+
 const zoneHysterisisRange DegF = 3 // DegF above and below the zone's configured ranges
-const chillerLockoutTemp DegF = 68 // DegF, if too cold, don't run chiller
-const chillerRecoveryTemp DegF = 75
-const boilerLockoutTemp DegF = 78
-const boilerREcoveryTemp DegF = 68
+
+const chillerLockoutTemp DegF = 68  // DegF, if too cold, don't run chiller
+const chillerRecoveryTemp DegF = 75 // all rooms must be above this temp to unlock the chiller
+const boilerLockoutTemp DegF = 78   // if a room is warmer than this, stop all heater pumps
+const boilerRecoveryTemp DegF = 68  // all rooms must fall below this to unlock the heater pumps
