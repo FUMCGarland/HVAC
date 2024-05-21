fd19a33a4ea7ccc3a8a2c4ceb52bde0276318dfc
Scot Bontrager <cloudkucooland@github.com>
2024-05-21 18:01:33 -0500

    add provision script

---
+++ scripts/shelly_provision.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+set mqtt_pass="sensor"
+
+curl http://$1/settings
+echo 
+curl http://$1/settings/cloud\?enabled=0
+curl http://$1/settings\?mqtt_enable=true\&mqtt_server=10.0.0.45:1883\&mqtt_user=sensor\&mqtt_pass=sensor\&discoverable=true\&mqtt_update_period=0\&temperature_units=F
+echo
+curl http://$1/reboot
+
