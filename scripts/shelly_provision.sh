efda6499db63aaffc6b31991fea057b250e04ab7
Scot Bontrager <cloudkucooland@github.com>
2024-05-22 22:43:10 -0500

    clean up

--- scripts/shelly_provision.sh
+++ scripts/shelly_provision.sh
@@ -1,11 +1,7 @@
 #/bin/bash
 
-set mqtt_pass="sensor"
-
-curl http://$1/settings
-echo 
 curl http://$1/settings/cloud\?enabled=0
-curl http://$1/settings\?mqtt_enable=true\&mqtt_server=10.0.0.45:1883\&mqtt_user=sensor\&mqtt_pass=sensor\&discoverable=true\&mqtt_update_period=0\&temperature_units=F\&mqtt_update_period=3600
-echo
+curl http://$1/settings\?mqtt_enable=true\&mqtt_server=10.0.0.45:1883\&mqtt_user=sensor\&mqtt_pass=sensor\&discoverable=true\&temperature_units=F\&mqtt_update_period=3600
 curl http://$1/reboot
+echo
 
