15eee7222cd0c0945d9250d9a69fade5a4191749
Scot Bontrager <scot@indievisible.org>
2024-06-04 15:04:25 -0500

    cleanup

--- relay.go
+++ relay.go
@@ -4,6 +4,8 @@ import (
 	"time"
 )
 
+// Relay is the primary config type for the relay-module process, set ONE of PumpID, BlowerID or ChillerID
+// the PIN is the GPIO pin which is determined by the device hardware
 type Relay struct {
 	StartTime time.Time     // time of current start
 	StopTime  time.Time     // time to stop running
@@ -12,6 +14,5 @@ type Relay struct {
 	PumpID    PumpID        // non-zero if devices is a pump
 	BlowerID  BlowerID      // non-zero if device is a blower
 	ChillerID ChillerID     // non-zero if the device is a chiller
-
-	Running bool // is the device currently running
+	Running   bool          // is the device currently running
 }
