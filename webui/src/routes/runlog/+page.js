248a379c477c888f0844ee3140a023695c738faf
Scot Bontrager <cloudkucooland@github.com>
2024-12-26 20:00:33 +0000

    remove debug

---
+++ webui/src/routes/runlog/+page.js
@@ -0,0 +1,7 @@
+import { hvaccontroller, genRequest } from '$lib/hvac';
+
+export async function load({ fetch, params }) {
+	const res = await fetch(`${hvaccontroller}/api/v1/runlog`, genRequest());
+	const item = await res.text();
+	return {data: item};
+}
---
+++ webui/src/routes/runlog/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import {
+		Heading,
+	} from 'flowbite-svelte';
+
+	export let data;
+</script>
+
+<Heading tag="h2">Run Log</Heading>
+<pre>{data.data}</pre>
